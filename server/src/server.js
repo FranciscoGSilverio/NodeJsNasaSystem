d8db5c95657cea35c9cb6f5bf6a45b5207773899
FranciscoGSilverio <franciscogsilverio@gmail.com>
2023-01-19 16:21:22 -0300

    Added the connection to mongoDb

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,36 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const { loadPlanets } = require("./models/planets.model");
 
-const PORT = process.env.PORT || 8000;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 9000;
 
 const server = http.createServer(app);
 
+const MONGO_URL = process.env.MONGODB_URL;
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDb connected succesfully");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(error);
+});
+
 const startup = async () => {
+  await mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
   await loadPlanets();
   server.listen(PORT, () => {
     console.log(`Server up and running on port ${PORT}`);
   });
-}
+};
 
 startup();
